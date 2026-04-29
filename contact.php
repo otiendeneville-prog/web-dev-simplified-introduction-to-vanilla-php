2b097518351ea9071fd41361b7af62394b66790c
Neville <otiendeneville@gamil.com>
2026-04-29 22:27:58 +0300

    setup: first of many commits

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+include("header.html")
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>This is Contact Page since it contains all the information concerning the contacts of the individuals in the series!</h1>
+</body>
+</html>
+
+<?php
+include("footer.html")
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    This the home Page
+    <a href="contact.php">This goes to contact page</a>
+</body>
+</html>
