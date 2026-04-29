542cbfddf2a5301c2ee1d7f9cc52d5a1c5bd8378
Neville <otiendeneville@gamil.com>
2026-04-29 23:03:17 +0300

    setup: successfully connected to the mysql database

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 
 <?php
 session_start();
+$host = "localhost";
+$user = "root";
+$password ="magynevy@11055";
+$database ="web_dev_simplified_introduction_to_vanilla_php";
+
+$connect = new mysqli($host,$user,$password,$database);
+$command = 'select * from users';
+$result = $connect->query($command);
+while($row = $result->fetch_assoc()){
+    print_r($row);
+    echo "<br>";
+}
 ?>
 
 <!DOCTYPE html>
