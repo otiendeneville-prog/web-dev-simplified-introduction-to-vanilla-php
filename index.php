cff3fb4fb74175f7a07f839c86f4af6e98c36704
Neville <otiendeneville@gamil.com>
2026-04-30 10:08:37 +0300

    post-variabe

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="index.php" method="get">
+    <form action="index.php" method="post">
 <label>Username:</label><br>
 <input type="text" name="username"><br>
 <label>Password:</label><br>
@@ -18,6 +18,6 @@
 
 <?php
 
- echo "{$_GET["username"]}<br>";
- echo "{$_GET["password"]}<br>";
+ echo "{$_POST["username"]}<br>";
+ echo "{$_POST["password"]}<br>";
 ?>
