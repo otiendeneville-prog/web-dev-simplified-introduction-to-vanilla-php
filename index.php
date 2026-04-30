047d587562d61337e8fe0210c8585ead87823946
Neville <otiendeneville@gamil.com>
2026-04-30 14:36:42 +0300

    addition of absolute

--- index.php
+++ index.php
@@ -1,26 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-   <form action="index.php" method="post">
-<label>quantity:</label>
-<input type="text" name="quantity"><br>
-<input type="submit" value="total">
-   </form>
+    <form action="index.html" method="post">
+        <label>x:</label>
+        <input type="text" name="x">
+        <input type="submit" value="total">
+    </form>
 </body>
 </html>
 
 <?php
-$item = "pizza";
-$price = 34.34;
-$quantity = $_POST["quantity"];
-$total = null;
 
-$total = $quantity * $price;
- echo"You have ordered {$quantity}  {$item} /s <br>";
- echo "Your total is: \${$total}"
+$x = $_POST["x"];
+$total = null;
+$total = abs($x);
+echo $total;
 ?>
