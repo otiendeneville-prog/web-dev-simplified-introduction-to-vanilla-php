5c3d62139c23912c9ac97b814168fa75dd77a3a2
Neville <otiendeneville@gamil.com>
2026-04-30 08:24:40 +0300

    addition of float

--- index.php
+++ index.php
@@ -9,6 +9,12 @@ $tax_rate = 5.21;
 $price = 23.34;
 
 
+$online = true;
+$employed = false;
+$crazy = true;
+
+
+
 echo "My name is {$name} <br>";
 echo "I am {$age} <br>";
 echo "I like {$food}";
@@ -16,5 +22,9 @@ echo "I like {$food}";
 
 echo"You gpa is {$gpa} <br>";
 echo"The tax rate currently is {$tax_rate}% <br>";
-echo"The price is \${$price}";
+echo"The price is \${$price} <br>";
+echo "Are you online:{$online} <br>";
+echo "Woo man that tutorial was crazy insane is it?{$crazy} <br>";
+
+echo"You have ordered {$food} at {$price}";
 ?>
