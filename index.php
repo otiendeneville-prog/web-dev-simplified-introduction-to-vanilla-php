c14d23959fbd21eed8b652e5eb3c837b8f870a90
Neville <otiendeneville@gamil.com>
2026-05-01 08:31:24 +0300

    calculation of weekly pay

--- index.php
+++ index.php
@@ -1,25 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <form action="index.html" method="post">
-        <label>x:</label>
-        <input type="text" name="x">
-        <input type="submit" value="total">
-    </form>
-</body>
-</html>
 
 <?php
+$hours = 50;
+$rate = 15;
+$weekly_pay = null;
 
-$x = $_POST["x"];
-$total = null;
-$total = round($x);
-echo $total;
+if($hours <=0){
+    $weekly_pay = 0;
+}
+elseif($hours <= 40){
+    $weekly_pay = $hours * $rate; 
+}
+else{
+    $weekly_pay = ($rate * 40) +(($hours-40)) *($rate*1.5);
+}
+
+echo"You made \${$weekly_pay} this week"
+
 ?>
