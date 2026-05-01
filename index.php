fef62a56de2286f0442f3b2e43ed99686d7dc2f0
Neville <otiendeneville@gamil.com>
2026-05-01 08:56:06 +0300

    not operator

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 
 <?php
 
-$temp = -2;
+$temp = 10000;
+$cloudy = false;
 
 if ($temp <0 || $temp > 30){
-echo"The weather is bad";
+echo"The weather is bad. <br>";
 }
 else{
-    echo"The weather is good";
+    echo"The weather is good. <br>";
+}
+if(!$cloudy){
+    echo"It's Sunny.";
+}
+
+else{
+    echo"It's cloudy.";
 }
 ?>
