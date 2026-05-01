6bc5210f3de0c710126ffe40fc5c548b0c6adaed
Neville <otiendeneville@gamil.com>
2026-05-01 10:38:46 +0300

    addition of brakes

--- index.php
+++ index.php
@@ -2,16 +2,38 @@
 <?php
 
 $date = date("l");
-$date = "Tuesday";
+$date = "Sunday";
 
-switch($date){
+switch ($date) {
     case "Monday":
-    echo"I hate Monday";
-    break;
+        echo "I hate Monday";
+        break;
 
-    case"Tuesday":
-    echo"It's tacco Tuesday";
-    break;
+    case "Tuesday":
+        echo "It's tacco Tuesday";
+        break;
+
+    case "Wednesday":
+        echo "Work week is halfway";
+        break;
+
+    case "Thursday":
+        echo "The weekend is almost here";
+        break;
+
+    case "Friday":
+        echo "Its almost the weekend";
+        break;
+
+    case "Saturday":
+        echo "Its time to party";
+        break;
+
+    case "Sunday":
+        echo "Time to relax";
+
+    default:
+        echo "That's not a valid day";
+        break;
 }
-    
 ?>
