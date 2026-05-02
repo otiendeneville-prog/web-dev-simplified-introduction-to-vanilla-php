d60110c7458d6ea0a4a01d9c85e0193fcc5d0282
Neville <otiendeneville@gamil.com>
2026-05-02 08:49:44 +0300

    adding counter in forloop

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label>Enter a to Count:</label><br>
+        <input type="text" name="counter"><br>
+        <input type="submit" value="start">
+    </form>
+</body>
+</html>
+
 
 <?php
+$counter = $_POST["counter"];
 
-for($i=0;$i <5;$i++){
-echo $i. "<br>";
+for($i = 0;$i <=$counter;$i++){
 
+echo $i ."<br>";
 }
 
 
