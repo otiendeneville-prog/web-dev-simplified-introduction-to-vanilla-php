e03e6e6d1c5b6158da52aba775fc56c3f4fa6ad6
Neville <otiendeneville@gamil.com>
2026-05-02 11:16:31 +0300

    testing array_reverse

--- index.php
+++ index.php
@@ -3,9 +3,12 @@ $capitals = array("USA"=>"Washington DC",
 "Japan"=>"Kyoto",
 "South Korea"=>"Seaul",
 "India"=>"New Delhi");
+$capitals = array_reverse($capitals);
 
 foreach($capitals as $key =>$value){
     echo "{$key} ={$value} <br>";
 
 }
 ?>
+
+
