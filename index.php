e4ca40248a7ec2b49aad992593136a0de14a1d80
Neville <otiendeneville@gamil.com>
2026-05-02 11:52:15 +0300

    button displaying capital of asseciate of an array

--- index.php
+++ index.php
@@ -1,14 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <form action="index.php" method="post">
+    <label>Enter the capital city:</label>
+    <input type="text" name="country"><br>
+    <input type="submit" value="Enter">
+   </form> 
+</body>
+</html>
+
+
+
 <?php
 $capitals = array("USA"=>"Washington DC",
 "Japan"=>"Kyoto",
 "South Korea"=>"Seaul",
 "India"=>"New Delhi");
-$capitals = array_reverse($capitals);
 
-foreach($capitals as $key =>$value){
-    echo "{$key} ={$value} <br>";
+$capital = $capitals[$_POST["country"]];
 
-}
+echo"The capital is {$capital}";
+
 ?>
 
 
