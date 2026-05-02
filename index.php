50f15cd0669124bac3bcfa6dcb6873d6a28250d2
Neville <otiendeneville@gamil.com>
2026-05-02 08:31:01 +0300

    increment of i

--- index.php
+++ index.php
@@ -1,39 +1,10 @@
 
 <?php
 
-$date = date("l");
-$date = "pizza";
+for($i=0;$i <5;$i++){
+echo $i. "<br>";
 
-switch ($date) {
-    case "Monday":
-        echo "I hate Monday";
-        break;
-
-    case "Tuesday":
-        echo "It's tacco Tuesday";
-        break;
-
-    case "Wednesday":
-        echo "Work week is halfway";
-        break;
-
-    case "Thursday":
-        echo "The weekend is almost here";
-        break;
-
-    case "Friday":
-        echo "Its almost the weekend";
-        break;
-
-    case "Saturday":
-        echo "Its time to party";
-        break;
-
-    case "Sunday":
-        echo "Time to relax <br>";
-
-    default:
-        echo "Not a valid day, enter a valid day";
-        break;
 }
+
+
 ?>
