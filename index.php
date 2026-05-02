875fc01c735496570da9f87c6dd4f46db16ef08d
Neville <otiendeneville@gamil.com>
2026-05-02 09:07:09 +0300

    foreach-loop

--- index.php
+++ index.php
@@ -1,26 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <label>Enter a to Count:</label><br>
-        <input type="text" name="counter"><br>
-        <input type="submit" value="start">
-    </form>
-</body>
-</html>
-
-
 <?php
-$counter = $_POST["counter"];
+$foods = array("apple","banana","orange","coconut");
 
-for($i = 0;$i <=$counter;$i++){
-
-echo $i ."<br>";
+foreach($foods as $food){
+    echo $food ."<br>";
 }
 
 
