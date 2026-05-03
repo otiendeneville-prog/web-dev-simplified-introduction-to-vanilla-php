9831ec018174cf2855f8e70f1f357f121763ac7c
Neville <otiendeneville@gamil.com>
2026-05-03 08:04:24 +0300

    addition of issest function

--- index.php
+++ index.php
@@ -1,34 +1,19 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-   <form action="index.php" method="post">
-    <label>Enter the capital city:</label>
-    <input type="text" name="country"><br>
-    <input type="submit" value="Enter">
-   </form> 
-</body>
-</html>
 
 
 
 <?php
-$capitals = array("USA"=>"Washington DC",
-"Japan"=>"Kyoto",
-"South Korea"=>"Seaul",
-"India"=>"New Delhi",
-"Kenya" =>"Nairobi",
-);
 
-$capital = $capitals[$_POST["country"]];
+$username = "true";
 
-echo"The capital is {$capital}";
+if(isset($username)){
+echo "The variable is valid";
+}
+else{
+    echo"The variable is Not valid";
+}
+
+
+
 
 ?>
-
-
