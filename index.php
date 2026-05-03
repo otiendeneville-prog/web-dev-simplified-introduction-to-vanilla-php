9ab41c8696f38e0c1254aea3416d0d9a52a81bb8
Neville <otiendeneville@gamil.com>
2026-05-03 10:07:08 +0300

    configuring checkboxes

--- index.php
+++ index.php
@@ -1,30 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <form action="index.php" method="post">
-       <input type="radio" name="credit_card" value="Visa">Visa.<br>
-       <input type="radio" name="credit_card" value="Mastercard">Mastercard.<br>
-       <input type="radio" name="credit_card" value="American Express">American Express.<br> 
-       <input type="submit" name="confirm" value="confirm">   
+        <input type="checkbox" name="pizza" value="Pizza">Pizza.<br>
+        <input type="checkbox" name="orange" value="Orange"> Orange.<br>
+        <input type="checkbox" name="neuville" value="Neuville"> Neuville.<br>
+        <input type="checkbox" name="tessy" value="Tessy">Tessy. <br>
+        <input type="submit" name="submit">
     </form>
 </body>
+
 </html>
 
 <?php
 
-if(isset($_POST["confirm"])){
+if(isset($_POST["submit"])){
+    if (isset($_POST["pizza"])) {
+    echo  "You like pizza <br>";
+}
+if(isset($_POST["orange"])){
+echo"You like Orange <br>";
+}
+if(isset($_POST["neuville"])){
+    echo"You like Ell <br>";
+}
+if(isset($_POST["tessy"])){
+echo"Are you Tessy? <br>";
+}
 
-if(isset($_POST["credit_card"])){
-    $credit_card = $_POST["credit_card"];
-    echo $credit_card;
-}
-else{
-    echo"Please make a selection to enter:";
-}
-}
+
+
+};
+
+
+
+
+
 ?>
