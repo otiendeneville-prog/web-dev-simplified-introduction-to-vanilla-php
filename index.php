24418ce85f47c17bb9ccf7bcab043b57f0ba1c6d
Neville <otiendeneville@gamil.com>
2026-05-03 10:45:49 +0300

    functions dublications

--- index.php
+++ index.php
@@ -1,46 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <form action="index.php" method="post">
-        <input type="checkbox" name="pizza" value="Pizza">Pizza.<br>
-        <input type="checkbox" name="orange" value="Orange"> Orange.<br>
-        <input type="checkbox" name="neuville" value="Neuville"> Neuville.<br>
-        <input type="checkbox" name="tessy" value="Tessy">Tessy. <br>
-        <input type="submit" name="submit">
-    </form>
-</body>
-
-</html>
-
 <?php
 
-if(isset($_POST["submit"])){
-    if (isset($_POST["pizza"])) {
-    echo  "You like pizza <br>";
-}
-if(isset($_POST["orange"])){
-echo"You like Orange <br>";
-}
-if(isset($_POST["neuville"])){
-    echo"You like Ell <br>";
-}
-if(isset($_POST["tessy"])){
-echo"Are you Tessy? <br>";
+function happy_birthday ($first_name, $age){
+   echo"Happy Birthday dear {$first_name}<br>";
+   echo"Happy Birthday to you <br>";
+   echo"You are {$age} years old <br>";
+   echo"Happy Birthday dear{$first_name} <br>";
 }
 
+happy_birthday("Sponge bob",22);
 
+happy_birthday("Terry",12);
 
-};
-
-
-
-
+happy_birthday("Mollina",34);
 
 ?>
+
+
