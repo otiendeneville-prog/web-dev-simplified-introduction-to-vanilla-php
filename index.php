3caeb8c340c78cfadec4f2d0feab351c3261d9ae
Neville <otiendeneville@gamil.com>
2026-05-04 08:24:55 +0300

    str_replace

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 
 $username = "Bro Code";
+$phonenumber = 134-553-323;
+
+
 $username = strtolower("Bro Code");
 $username = strtoupper("Bro Code");
+$phonenumber = str_replace("_", "", $phonenumber);
 
-echo $username;
+echo $phonenumber;
 
 
 ?>
