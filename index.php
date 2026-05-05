5051ccca6b7c6b3fbd307dc7419bc50bc389424c
Neville <otiendeneville@gamil.com>
2026-05-05 10:37:16 +0300

    password verify in the browser only

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 $password = "Neuville@11055";
 $harsh = password_hash($password, PASSWORD_DEFAULT);
 
-echo $harsh."<br>";
+
 
 if(password_verify( "Neuville@11055", $harsh)){
 echo"You are loged in";
