a389e568e7b23dac95a9d79e6ea72991ce8e3ab4
Neville <otiendeneville@gamil.com>
2026-05-05 18:38:37 +0300

    configuring xammp to mysql database

---
+++ database.php
@@ -0,0 +1,21 @@
+<?php
+$db_server = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "businessdb";
+$conn = "";
+
+
+try{
+$conn = mysqli_connect($db_server,
+$db_user,
+$db_pass,
+$db_name);
+}
+catch(mysqli_sql_exception){
+  echo"You are connected!";
+};
+
+if($conn){
+    echo"You are not connected";
+};
