4007ed72e208ddcb4238f6812563e24ba2ba313b
Neville <otiendeneville@gamil.com>
2026-05-05 10:35:54 +0300

    determing password verifyer

--- index.php
+++ index.php
@@ -4,9 +4,14 @@
 $password = "Neuville@11055";
 $harsh = password_hash($password, PASSWORD_DEFAULT);
 
-echo $harsh;
+echo $harsh."<br>";
 
-
+if(password_verify( "Neuville@11055", $harsh)){
+echo"You are loged in";
+}
+else{
+    echo"Incorrect password!";
+}
 ?>
 
 
