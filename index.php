1bcc3c1c527648baaa65c38c3b0d7947fe1009f0
Neville <otiendeneville@gamil.com>
2026-05-06 21:14:43 +0300

    adding filte method

--- index.php
+++ index.php
@@ -1,29 +1,50 @@
 
-
 <?php 
 
 include("database.php");
 
-$username ="Neuville";
-$password = "clarity13";
-$hash = password_hash($password,PASSWORD_DEFAULT);
 
 
-$sql = "INSERT INTO users(user,password)
-                   VALUES('$username','$hash')";
 
-try{
-    mysqli_query($conn, $sql);
-    echo"User is now registered";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
+    <h2>Welcome to Fakebook</h2>
+    Username: <br>
+    <input type="text" name="username"><br>
+
+    Password:<br>
+    <input type="password" name="password"><br>
+    <input type="submit" name="submit" value="register">
+</form>
+    
+</body>
+</html>
+
+<?php
+
+if($_SERVER["REQUEST_METHOD"] =="POST"){
+$username = filter_input(INPUT_POST, "username",FILTER_SANITIZE_SPECIAL_CHARS);
+$password = filter_input(INPUT_POST,"password", FILTER_SANITIZE_SPECIAL_CHARS);
+}
+
+if(empty($username)){
+    echo"Please type in your username";
 
 }
-catch(mysqli_sql_exception){
-  echo"Could not register user";
+if(empty($password)){
+    echo"Please type in you password";
 }
-
-
-mysqli_close($conn);
-
+mysqli_close($conn); 
 
 
 ?>
